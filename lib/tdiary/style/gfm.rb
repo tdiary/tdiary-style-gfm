cabb959e8f1ff8bde2fcc74c5b12e89cd78c5256
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-08-26 13:06:52 +0900

    bundle gem tdiary-style-gfm -t

---
+++ lib/tdiary/style/gfm.rb
@@ -0,0 +1,9 @@
+require "tdiary/style/gfm/version"
+
+module Tdiary
+  module Style
+    module Gfm
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/tdiary/style/gfm/version.rb
@@ -0,0 +1,7 @@
+module Tdiary
+  module Style
+    module Gfm
+      VERSION = "0.0.1"
+    end
+  end
+end
