cb3304519cf0d877f34f2ea6be894d0d79a9b8a1
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-13 15:38:03 +0900

    rake spec is works

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in tdiary-style-gfm.gemspec
 gemspec
+
+gem 'tdiary', github: 'tdiary/tdiary-core'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'tdiary/comment_manager'
+require 'tdiary/referer_manager'
+require 'tdiary/style'
 require 'tdiary/style/gfm'
