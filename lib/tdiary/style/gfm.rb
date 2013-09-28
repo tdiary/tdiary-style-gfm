b5b4fe03967f86c8509d01426befec87586f76b7
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2013-09-28 11:47:18 +0900

    remove comments

--- lib/tdiary/style/gfm.rb
+++ lib/tdiary/style/gfm.rb
@@ -1,17 +1,4 @@
 # -*- coding: utf-8; -*-
-#
-# gfm_style.rb: "GitHub Flavored Markdown" (GFM) style for tDiary 2.x format.
-#
-# if you want to use this style, add @style into tdiary.conf below:
-#
-#    @style = 'GFM'
-#
-# Copyright (C) 2003, TADA Tadashi <sho@spc.gr.jp>
-# Copyright (C) 2004, MoonWolf <moonwolf@moonwolf.com>
-# Copyright (C) 2012, kdmsnr <kdmsnr@gmail.com>
-# Copyright (C) 2013, hsbt <shibata.hiroshi@gmail.com>
-# You can distribute this under GPL.
-#
 
 require 'redcarpet'
 require 'pygments'
